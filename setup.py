77f8b48287b69e4db7c6dfabe18d7e158812773e
Thibault Clérice <leponteineptique@gmail.com>
2025-07-13 12:58:55 +0200

    0.0.1a2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
 
 setup(
     name="dapytains",
-    version="0.0.1a1",
+    version="0.0.1a2",
     author="Thibault Clérice",
     author_email="thibault.clerice@inria.fr",
     description="A brief description of dapytains",
